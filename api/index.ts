51d14b9be7359c29d7923be6e7ce559c38aea365
Sebastian Krüger <2pi_r2@gmx.de>
2021-01-05 15:27:36 +0100

    fix

--- api/index.ts
+++ api/index.ts
@@ -94,7 +94,9 @@ app.post('/anmeldung/ma/ort', (req, res) => {
 app.post('/anmeldung/ma/veranstaltung', async (req, res) => {
   const tk: string = (await checkToken(req.body.token)).d
 
-  const [veranstaltungsID, position] = tk.split('|').map((v) => parseInt(v))
+  const [veranstaltungsID, position] = tk
+    .split('|')
+    .map((v) => parseInt(v) || v)
 
   req.body.veranstaltungsID = veranstaltungsID
   req.body.position = position
