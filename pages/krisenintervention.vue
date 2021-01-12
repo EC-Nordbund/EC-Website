b0b42b8c323518554be8a8d720aa0c39ade9623f
Sebastian Krüger <2pi_r2@gmx.de>
2021-01-12 16:39:52 +0100

    fix

--- pages/krisenintervention.vue
+++ pages/krisenintervention.vue
@@ -1,118 +1,157 @@
 <template lang="pug">
-  v-container
-    v-row(justify="center")
-      v-col(cols="12" sm="10" md="8")
-        h1 Kinder- und Jugendschutz
-        h4.text-subtitle-1 Jugendschutz, Kindeswohl und Krisenintervention – Diese Seite bietet erste Hilfe für alle Notfälle.
+v-container
+  v-row(justify='center')
+    v-col(cols='12', sm='10', md='8')
+      h1 Kinder- und Jugendschutz
+      h4.text-subtitle-1 Jugendschutz, Kindeswohl und Krisenintervention – Diese Seite bietet erste Hilfe für alle Notfälle.
 
-    //- Ansprechpartner
-    v-row(justify="center")
-      v-col(cols="12" sm="10" md="8").pb-0
-        h2.text-h5 Ansprechpartner
-      v-col(cols="12" sm="10" md="8")
+  //- Ansprechpartner
+  v-row(justify='center')
+    v-col.pb-0(cols='12', sm='10', md='8')
+      h2.text-h5 Ansprechpartner
+    v-col(cols='12', sm='10', md='8')
+      //- Thomas
+      v-card.mb-4(tile, outlined)
+        v-col.py-0(cols='12')
+          v-row
+            v-col.d-flex.flex-column.justify-center.align-center.ec-gradient.pa-3(cols='12', sm='auto')
+              v-img.flex-grow-0.hexagon-shape(
+                :src='require("~/assets/img/thomas_seeger.jpg")',
+                :width='128',
+                :height='128'
+              )
+            v-col.px-sm-5
+              h4.text-h6 Thomas Seeger
+              p.subtitle-1 Jugendreferent
 
-        //- Thomas
-        v-card(tile outlined).mb-4
-          v-col(cols="12").py-0
-            v-row
-              v-col(cols="12" sm="auto").d-flex.flex-column.justify-center.align-center.ec-gradient.pa-3
-                v-img.flex-grow-0.hexagon-shape(
-                  :src='require("~/assets/img/thomas_seeger.jpg")',
-                  :width='128',
-                  :height='128')
-              v-col.px-sm-5
-                h4.text-h6 Thomas Seeger
-                p.subtitle-1 Jugendreferent
+              //- Telefon
+              .d-flex.flex-row.align-center.mb-2
+                v-btn(
+                  fab,
+                  x-small,
+                  color='primary',
+                  depressed,
+                  href='tel:004945322805703'
+                )
+                  v-icon.ki-icon mdi-phone
+                span.text-subtitle-2.font-weight-medium.pl-3
+                  a.text-decoration-none(href='tel:004945322805703', title='Thomas Seeger anrufen')
+                    | 04532 280 57 03
 
-                //- Telefon
-                .d-flex.flex-row.align-center.mb-2
-                  v-btn(fab x-small color="primary" depressed href="tel:004945322805703")
-                    v-icon mdi-phone
-                  span.text-subtitle-2.font-weight-medium.pl-3
-                    a(href="tel:004945322805703" title="Thomas Seeger anrufen").text-decoration-none
-                      | 04532 280 57 03
+              //- E-Mail
+              .d-flex.flex-row.align-center
+                v-btn(
+                  fab,
+                  x-small,
+                  color='primary',
+                  depressed,
+                  href='mailto:referent@ec-nordbund.de'
+                )
+                  v-icon.ki-icon mdi-email
+                span.text-subtitle-2.font-weight-medium.pl-3
+                  a.text-decoration-none(
+                    href='mailto:referent@ec-nordbund.de',
+                    title='Thomas Seeger eine E-Mail schreiben'
+                  )
+                    | referent@ec-nordbund.de
 
-                //- E-Mail
-                .d-flex.flex-row.align-center
-                  v-btn(fab x-small color="primary" depressed href="mailto:referent@ec-nordbund.de")
-                    v-icon mdi-email
-                  span.text-subtitle-2.font-weight-medium.pl-3
-                    a(href="mailto:referent@ec-nordbund.de" title="Thomas Seeger eine E-Mail schreiben").text-decoration-none
-                      | referent@ec-nordbund.de
+      //-  Dortje
+      v-card.mb-4(tile, outlined)
+        v-col.py-0(cols='12')
+          v-row
+            v-col.d-flex.flex-column.justify-center.align-center.ec-gradient.pa-3(cols='12', sm='auto')
+              v-img.flex-grow-0.hexagon-shape(
+                :src='require("~/assets/img/dortje_gaertner.jpg")',
+                :width='128',
+                :height='128'
+              )
+            v-col.px-sm-5
+              h4.text-h6 Dortje Gaertner
+              p.subtitle-1 Kinder- und Jungschararbeit
 
-        //-  Dortje
-        v-card(tile outlined).mb-4
-          v-col(cols="12").py-0
-            v-row
-              v-col(cols="12" sm="auto").d-flex.flex-column.justify-center.align-center.ec-gradient.pa-3
-                v-img.flex-grow-0.hexagon-shape(
-                  :src='require("~/assets/img/dortje_gaertner.jpg")',
-                  :width='128',
-                  :height='128')
-              v-col.px-sm-5
-                h4.text-h6 Dortje Gaertner
-                p.subtitle-1 Kinder- und Jungschararbeit
+              //- Telefon
+              .d-flex.flex-row.align-center.mb-2
+                v-btn(fab, x-small, color='primary', depressed, href='tel:???')
+                  v-icon.ki-icon mdi-phone
+                span.text-subtitle-2.font-weight-medium.pl-3
+                  a.text-decoration-none(href='tel:00494122489036', title='Dortje Gaertner anrufen')
+                    | 04122 48 90 36
 
-                //- Telefon
-                .d-flex.flex-row.align-center.mb-2
-                  v-btn(fab x-small color="primary" depressed href="tel:???")
-                    v-icon mdi-phone
-                  span.text-subtitle-2.font-weight-medium.pl-3
-                    a(href="tel:00494122489036" title="Dortje Gaertner anrufen").text-decoration-none
-                      | 04122 48 90 36 
+              //- E-Mail
+              .d-flex.flex-row.align-center
+                v-btn(
+                  fab,
+                  x-small,
+                  color='primary',
+                  depressed,
+                  href='mailto:kinder-referent@ec-nordbund.de'
+                )
+                  v-icon.ki-icon mdi-email
+                span.text-subtitle-2.font-weight-medium.pl-3
+                  a.text-decoration-none(
+                    href='mailto:kinder-referent@ec-nordbund.de',
+                    title='Dortje Gaertner eine E-Mail schreiben'
+                  )
+                    | kinder-referent@ec-nordbund.de
 
-                //- E-Mail
-                .d-flex.flex-row.align-center
-                  v-btn(fab x-small color="primary" depressed href="mailto:kinder-referent@ec-nordbund.de")
-                    v-icon mdi-email
-                  span.text-subtitle-2.font-weight-medium.pl-3
-                    a(href="mailto:kinder-referent@ec-nordbund.de" title="Dortje Gaertner eine E-Mail schreiben").text-decoration-none
-                      | kinder-referent@ec-nordbund.de
+      //-  Jutta
+      v-card(tile, outlined)
+        v-col.py-0(cols='12')
+          v-row
+            v-col.d-flex.flex-column.justify-center.align-center.ec-gradient.pa-3(cols='12', sm='auto')
+              v-img.flex-grow-0.hexagon-shape(
+                :src='require("~/assets/img/Jutta-Nordsiek_1301x974[1].jpg")',
+                :width='128',
+                :height='128'
+              )
+            v-col.px-sm-5
+              h4.text-h6 Jutta Nordsiek
+              p.subtitle-1 Seelsorge und Beratungsstelle des VG
 
-        //-  Jutta
-        v-card(tile outlined)
-          v-col(cols="12").py-0
-            v-row
-              v-col(cols="12" sm="auto").d-flex.flex-column.justify-center.align-center.ec-gradient.pa-3
-                v-img.flex-grow-0.hexagon-shape(
-                  :src='require("~/assets/img/Jutta-Nordsiek_1301x974[1].jpg")',
-                  :width='128',
-                  :height='128')
-              v-col.px-sm-5
-                h4.text-h6 Jutta Nordsiek
-                p.subtitle-1 Seelsorge und Beratungsstelle des VG
+              //- Telefon
+              .d-flex.flex-row.align-center.mb-2
+                v-btn(fab, x-small, color='primary', depressed, href='tel:???')
+                  v-icon.ki-icon mdi-phone
+                span.text-subtitle-2.font-weight-medium.pl-3
+                  a.text-decoration-none(href='tel:004941224048694', title='Jutta Nordsiek anrufen')
+                    | 04122 404 86 94
 
-                //- Telefon
-                .d-flex.flex-row.align-center.mb-2
-                  v-btn(fab x-small color="primary" depressed href="tel:???")
-                    v-icon mdi-phone
-                  span.text-subtitle-2.font-weight-medium.pl-3
-                    a(href="tel:004941224048694" title="Jutta Nordsiek anrufen").text-decoration-none
-                      | 04122 404 86 94
+              //- E-Mail
+              .d-flex.flex-row.align-center
+                v-btn(
+                  fab,
+                  x-small,
+                  color='primary',
+                  depressed,
+                  href='mailto:jutta.nordsiek@vg-sh.de'
+                )
+                  v-icon.ki-icon mdi-email
+                span.text-subtitle-2.font-weight-medium.pl-3
+                  a.text-decoration-none(
+                    href='mailto:jutta.nordsiek@vg-sh.de',
+                    title='Jutta Nordsiek eine E-Mail schreiben'
+                  )
+                    | jutta.nordsiek@vg-sh.de
 
-                //- E-Mail
-                .d-flex.flex-row.align-center
-                  v-btn(fab x-small color="primary" depressed href="mailto:jutta.nordsiek@vg-sh.de")
-                    v-icon mdi-email
-                  span.text-subtitle-2.font-weight-medium.pl-3
-                    a(href="mailto:jutta.nordsiek@vg-sh.de" title="Jutta Nordsiek eine E-Mail schreiben").text-decoration-none
-                      | jutta.nordsiek@vg-sh.de
+    //- Explanation
+    v-col(cols='12', sm='10', md='8')
+      nuxt-content(:document='explanation')
 
-      //- Explanation
-      v-col(cols="12" sm="10" md="8")
-        nuxt-content(:document="explanation")
-
-      //- Downloads
-      v-col(cols="12" sm="10" md="8").pb-0
-        h2.text-h5 Downloads
-      v-col(cols="12" sm="10" md="8")
-        v-list(color="#eee").py-0
-          v-list-item(v-for="file in data.files" :key="'file_' + file.filename"  :href="`${file.filename}`").px-1
-            v-list-item-avatar
-              v-icon(size="32") {{ {pdf: mdiFilePdfOutline,docx: mdiFileWord,jpg: mdiFileImage,png: mdiFileImage}[file.filename.split('.')[1].toLowerCase()] || mdiFile }}
-            v-list-item-content
-              v-list-item-title {{file.title}}
-              v-list-item-subtitle {{file.description}}
+    //- Downloads
+    v-col.pb-0(cols='12', sm='10', md='8')
+      h2.text-h5 Downloads
+    v-col(cols='12', sm='10', md='8')
+      v-list.py-0(color='#eee')
+        v-list-item.px-1(
+          v-for='file in data.files',
+          :key='"file_" + file.filename',
+          :href='`${file.filename}`'
+        )
+          v-list-item-avatar
+            v-icon(size='32') {{ { pdf: mdiFilePdfOutline, docx: mdiFileWord, jpg: mdiFileImage, png: mdiFileImage }[file.filename.split(".")[1].toLowerCase()] || mdiFile }}
+          v-list-item-content
+            v-list-item-title {{ file.title }}
+            v-list-item-subtitle {{ file.description }}
 </template>
 <script lang="ts">
 import {
@@ -197,5 +236,8 @@ export default defineComponent({
 <style lang="scss" scoped>
 .hexagon-shape {
   clip-path: polygon(50% 100%, 5% 75%, 5% 25%, 50% 0%, 95% 25%, 95% 75%);
+}
+.ki-icon {
+  padding: 3px;
 }
 </style>
--- postCssProcessing.mjs
+++ postCssProcessing.mjs
@@ -13,10 +13,8 @@ function handleDir(dir) {
     } else {
       handleDir(combined)
     }
-
   })
 }
-
 
 function handleFile(file) {
   let content = readFileSync(file, 'utf-8')
@@ -34,23 +32,50 @@ function handleFile(file) {
 
 // TODO: optimize multiple css selectors
 function handleContents(content) {
-  return content.split('}')
-    .filter(v => {
+  return content
+    .split('}')
+    .filter((v) => {
       const sel = v.split('{')[0]
       // sel.includes(':not') ||
-      return sel.split(',').every(selector =>
-        !selector.includes('.v-application--is-rtl') && !selector.includes('.v-toolbar__items') && !selector.includes('[dir=rtl]') && !(selector.includes('rounded') && !selector.includes('0')) &&
-        !selector.includes('.v-navigation-drawer--mini-variant') && !selector.includes('.v-text-field--enclosed') && !selector.includes('.v-text-field--outlined') &&
-        !selector.includes('.v-select--chips') && !selector.includes('.v-input--dense') && !selector.includes('v-toolbar--prominen') &&
-        !selector.includes('order') && !selector.includes('.v-card--disabled') && !selector.includes('.v-app-bar--fixed') && !selector.includes('.v-app-bar--fade-img-on-scroll') &&
-        !selector.includes('.v-toolbar__content>.v-tabs') && !selector.includes('.v-toolbar__image') && !selector.includes('.v-progress-linear--reverse') && !selector.includes('.v-card--disabled') &&
-        !selector.includes('.v-pagination--disabled') && !selector.includes('.container.grid-list-') && !selector.includes('.layout.reverse') && !selector.includes('.v-icon--dense') &&
-        !selector.includes('.v-progress-circular') && !selector.includes('.v-icon--disabled') && !selector.includes('.v-btn--fab') &&
-        !selector.includes('.theme--dark'))
+      return sel
+        .split(',')
+        .every(
+          (selector) =>
+            !selector.includes('.v-application--is-rtl') &&
+            !selector.includes('.v-toolbar__items') &&
+            !selector.includes('[dir=rtl]') &&
+            !(selector.includes('rounded') && !selector.includes('0')) &&
+            !selector.includes('.v-navigation-drawer--mini-variant') &&
+            !selector.includes('.v-text-field--enclosed') &&
+            !selector.includes('.v-text-field--outlined') &&
+            !selector.includes('.v-select--chips') &&
+            !selector.includes('.v-input--dense') &&
+            !selector.includes('v-toolbar--prominen') &&
+            !selector.includes('order') &&
+            !selector.includes('.v-card--disabled') &&
+            !selector.includes('.v-app-bar--fixed') &&
+            !selector.includes('.v-app-bar--fade-img-on-scroll') &&
+            !selector.includes('.v-toolbar__content>.v-tabs') &&
+            !selector.includes('.v-toolbar__image') &&
+            !selector.includes('.v-progress-linear--reverse') &&
+            !selector.includes('.v-card--disabled') &&
+            !selector.includes('.v-pagination--disabled') &&
+            !selector.includes('.container.grid-list-') &&
+            !selector.includes('.layout.reverse') &&
+            !selector.includes('.v-icon--dense') &&
+            !selector.includes('.v-progress-circular') &&
+            !selector.includes('.v-icon--disabled') &&
+            !selector.includes('.v-btn--fab') &&
+            !selector.includes('.theme--dark')
+        )
     })
-    .join('}').split('  ').join('').split('\n ').join('\n').split('\n\n').join('\n')
-
-
+    .join('}')
+    .split('  ')
+    .join('')
+    .split('\n ')
+    .join('\n')
+    .split('\n\n')
+    .join('\n')
 }
 
 handleDir(base)
