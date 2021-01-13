3ea11c1eea0c0ab31ee4d8ec7ef34c8d7d38276f
Sebastian Krüger <2pi_r2@gmx.de>
2021-01-13 15:46:54 +0100

    add leaflet gesture handling (#27)
    * add gesture handling
    
    * add gesture handler to ec-location component
    
    * add prop to desiable gesture handling
    
    * verbesserungen
    
    Co-authored-by: Tobias <tobias_krause@akranet.de>
    Co-authored-by: Tobias Krause <39905423+tobikrs@users.noreply.github.com>

--- components/ecLocation.vue
+++ components/ecLocation.vue
@@ -1,23 +1,32 @@
 <template lang="pug">
-   client-only
-      l-map(:zoom="zoom" :center="marker[0].marker" class="overflow-hidden")
-        l-tile-layer(:email="`app@ec-nordbund.de`" url="https://{s}.tile.openstreetmap.de/{z}/{x}/{y}.png" attribution='&copy; <a href="https://osm.org/copyright">OpenStreetMap</a> contributors')
-        l-control-scale(position="topright" metric)
-        l-marker(v-for="m in marker" :lat-lng="m.marker" :key="m.title")
-          l-popup(:options="{keepInView:true,autoClose:false}")
-            p
-              b {{m.title}}
-              br
-              | {{m.strasse}}
-              br
-              | {{m.plz}} {{m.ort}}
-              br(v-if="m.email")
-              a(v-if="m.email" :href="`mailto:${m.email}`") {{m.email}}
-              br(v-if="!m.noMore")
-              nuxt-link(v-if="!m.noMore" :to="m.more" style="cursor: pointer") mehr...
+client-only
+  l-map(
+    :zoom='zoom',
+    :center='marker[0].marker',
+    gestureHandling,
+    :options='mapOptions'
+  ).overflow-hidden
+    l-tile-layer(
+      :email='`app@ec-nordbund.de`',
+      url='https://{s}.tile.openstreetmap.de/{z}/{x}/{y}.png',
+      attribution='&copy; <a href="https://osm.org/copyright">OpenStreetMap</a> contributors'
+    )
+    l-control-scale(position='topright', metric)
+    l-marker(v-for='m in marker', :lat-lng='m.marker', :key='m.title')
+      l-popup(:options='{ keepInView: true, autoClose: false }')
+        p
+          b {{ m.title }}
+          br
+          | {{ m.strasse }}
+          br
+          | {{ m.plz }} {{ m.ort }}
+          br(v-if='m.email')
+          a(v-if='m.email', :href='`mailto:${m.email}`') {{ m.email }}
+          br(v-if='!m.noMore')
+          nuxt-link(v-if='!m.noMore', :to='m.more', style='cursor: pointer') mehr...       
 </template>
 <script>
-import { defineComponent } from '@nuxtjs/composition-api'
+import { defineComponent, computed } from '@nuxtjs/composition-api'
 
 export default defineComponent({
   props: {
@@ -29,6 +38,18 @@ export default defineComponent({
       type: Number,
       default: 7,
     },
+    disableGestureHandling: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  setup(props) {
+    const mapOptions = computed(() => ({
+      zoomSnap: 0.5,
+      gestureHandling: !props.disableGestureHandling,
+    }))
+
+    return { mapOptions }
   },
 })
 </script>
