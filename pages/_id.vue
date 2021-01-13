6c7e0f65d28ccb8f0a9748e80d1b0e9a41957566
Tobias Krause <39905423+tobikrs@users.noreply.github.com>
2021-01-13 15:22:04 +0100

    Update Karlsminde Design (#24)
    * style karlsminde page
    
    * hide overflow on leaflet map
    
    * add page to nav
    
    * redirect /karlsminde
    
    * Update lighthouserc.js
    
    * Update lighthouserc.js
    
    * remove delimiters
    
    * fix telefon
    
    * fix rel on link
    
    * fix order
    
    * fix comment
    
    Co-authored-by: Sebastian Krüger <2pi_r2@gmx.de>

--- pages/_id.vue
+++ pages/_id.vue
@@ -1,10 +1,15 @@
 <template lang="pug">
-  v-container
-    | WENN DU DAS LIEST IST WAS SCHIEFGEGANGEN...
+v-container
+  | WENN DU DAS LIEST IST WAS SCHIEFGEGANGEN...
 </template>
 <script>
 export default {
   async asyncData({ $content, params, redirect, route }) {
+    if (params.id.toLowerCase() === 'karlsminde') {
+      redirect('/orte/karlsminde')
+      return
+    }
+
     const { slugs } = await $content('old-news-slugs').fetch()
     // console.log(slugs)
 
