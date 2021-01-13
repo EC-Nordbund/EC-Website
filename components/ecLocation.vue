fb904dd3f0038b6b9183132c290b9a7b5a0fdde7
Sebastian Krüger <2pi_r2@gmx.de>
2021-01-13 14:51:47 +0100

    verbesserungen

--- components/ecLocation.vue
+++ components/ecLocation.vue
@@ -1,23 +1,32 @@
 <template lang="pug">
-   client-only
-      l-map(:zoom="zoom" :center="marker[0].marker" gestureHandling :options="mapOptions")
-        l-tile-layer(:email="`app@ec-nordbund.de`" url="https://{s}.tile.openstreetmap.de/{z}/{x}/{y}.png" attribution='&copy; <a href="https://osm.org/copyright">OpenStreetMap</a> contributors')
-        l-control-scale(position="topright" metric)
-        l-marker(v-for="m in marker" :lat-lng="m.marker" :key="m.title")
-          l-popup(:options="{keepInView:true,autoClose:false}")
-            p
-              b {{m.title}}
-              br
-              | {{m.strasse}}
-              br
-              | {{m.plz}} {{m.ort}}
-              br(v-if="m.email")
-              a(v-if="m.email" :href="`mailto:${m.email}`") {{m.email}}
-              br(v-if="!m.noMore")
-              nuxt-link(v-if="!m.noMore" :to="m.more" style="cursor: pointer") mehr...
+client-only
+  l-map(
+    :zoom='zoom',
+    :center='marker[0].marker',
+    gestureHandling,
+    :options='mapOptions'
+  )
+    l-tile-layer(
+      :email='`app@ec-nordbund.de`',
+      url='https://{s}.tile.openstreetmap.de/{z}/{x}/{y}.png',
+      attribution='&copy; <a href="https://osm.org/copyright">OpenStreetMap</a> contributors'
+    )
+    l-control-scale(position='topright', metric)
+    l-marker(v-for='m in marker', :lat-lng='m.marker', :key='m.title')
+      l-popup(:options='{ keepInView: true, autoClose: false }')
+        p
+          b {{ m.title }}
+          br
+          | {{ m.strasse }}
+          br
+          | {{ m.plz }} {{ m.ort }}
+          br(v-if='m.email')
+          a(v-if='m.email', :href='`mailto:${m.email}`') {{ m.email }}
+          br(v-if='!m.noMore')
+          nuxt-link(v-if='!m.noMore', :to='m.more', style='cursor: pointer') mehr...
 </template>
 <script>
-import { defineComponent, ref, onMounted } from '@nuxtjs/composition-api'
+import { defineComponent, computed } from '@nuxtjs/composition-api'
 
 export default defineComponent({
   props: {
@@ -31,28 +40,14 @@ export default defineComponent({
     },
     disableGestureHandling: {
       type: Boolean,
-      default: false
-    }
+      default: false,
+    },
   },
   setup(props) {
-    const mapOptions = ref({
+    const mapOptions = computed(() => ({
       zoomSnap: 0.5,
       gestureHandling: !props.disableGestureHandling,
-    })
-
-    // add leaflet gesture handler
-    onMounted(() => {
-      if (!props.disableGestureHandling) {
-        const L = require('leaflet')
-        require('leaflet/dist/leaflet.css')
-
-        globalThis.L = L
-        const { GestureHandling } = require('leaflet-gesture-handling')
-        require('leaflet-gesture-handling/dist/leaflet-gesture-handling.css')
-
-        L.Map.addInitHook('addHandler', 'gestureHandling', GestureHandling)
-      }
-    })
+    }))
 
     return { mapOptions }
   },
