00de4ddac59ca4a196775284c0b8a11fdee32e73
Tobias <tobias_krause@akranet.de>
2021-01-13 14:25:48 +0100

    add gesture handler to ec-location component

--- components/ecLocation.vue
+++ components/ecLocation.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
    client-only
-      l-map(:zoom="zoom" :center="marker[0].marker")
+      l-map(:zoom="zoom" :center="marker[0].marker" gestureHandling :options="mapOptions")
         l-tile-layer(:email="`app@ec-nordbund.de`" url="https://{s}.tile.openstreetmap.de/{z}/{x}/{y}.png" attribution='&copy; <a href="https://osm.org/copyright">OpenStreetMap</a> contributors')
         l-control-scale(position="topright" metric)
         l-marker(v-for="m in marker" :lat-lng="m.marker" :key="m.title")
@@ -17,7 +17,7 @@
               nuxt-link(v-if="!m.noMore" :to="m.more" style="cursor: pointer") mehr...
 </template>
 <script>
-import { defineComponent } from '@nuxtjs/composition-api'
+import { defineComponent, onMounted } from '@nuxtjs/composition-api'
 
 export default defineComponent({
   props: {
@@ -29,6 +29,26 @@ export default defineComponent({
       type: Number,
       default: 7,
     },
+  },
+  setup() {
+    const mapOptions = {
+      zoomSnap: 0.5,
+      gestureHandling: true,
+    }
+
+    // add leaflet gesture handler
+    onMounted(() => {
+      const L = require('leaflet')
+      require('leaflet/dist/leaflet.css')
+
+      globalThis.L = L
+      const { GestureHandling } = require('leaflet-gesture-handling')
+      require('leaflet-gesture-handling/dist/leaflet-gesture-handling.css')
+
+      L.Map.addInitHook('addHandler', 'gestureHandling', GestureHandling)
+    })
+
+    return { mapOptions }
   },
 })
 </script>
