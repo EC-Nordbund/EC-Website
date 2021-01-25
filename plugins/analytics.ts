ab9735a409a3a7227dbe0e8a3a75c9a447f2e27c
Sebastian Krüger <2pi_r2@gmx.de>
2021-01-25 12:16:19 +0100

    update tracking

--- plugins/analytics.ts
+++ plugins/analytics.ts
@@ -1,21 +1,96 @@
-// @ts-ignore
-import { create } from 'ackee-tracker/src/scripts/main'
-import { defineNuxtPlugin } from '@nuxtjs/composition-api'
+/* eslint-disable no-console */
+import { Plugin } from '@nuxt/types'
+// @ts-expect-error no import types (pending PR DefinitelyTyped/DefinitelyTyped #50817)
+import { attributes, create } from 'ackee-tracker'
+import { getCLS, getFCP, getFID, getLCP, getTTFB } from 'web-vitals'
 
-export default defineNuxtPlugin((context) => {
-  if (navigator.doNotTrack != '1') {
-    const ackee = create(
-      {
-        server: 'https://analytics.ec-nordbund.de',
-        domainId: '7e04b501-fc99-417a-b65a-3fd7e412050c',
-      },
-      {
-        ignoreLocalhost: true,
-        detailed: true,
-      }
-    )
+const __URL__ = 'https://analytics.ec-nordbund.de'
+const __DOMAIN_ID__ = '7e04b501-fc99-417a-b65a-3fd7e412050c'
+const __WEB_VITALS_EVENT_ID__ = '2851d51a-0085-4405-8c41-1805cd7dd390'
+const __BOUNCE_EVENT_ID__ = '6a328551-2338-4b62-8b48-a99343baeab6'
 
-    // @ts-expect-error
-    context.app.router.afterEach(() => ackee.record())
+const webVitals: [
+  string,
+  (cb: (metric: { value: number }) => void) => void
+][] = [
+  ['CLS', getCLS],
+  ['FCP', getFCP],
+  ['FID', getFID],
+  ['LCP', getLCP],
+  ['TTFB', getTTFB],
+]
+
+const plugin: Plugin = (ctx) => {
+  // eslint-disable-next-line eqeqeq
+  const DO_TRACKING = navigator.doNotTrack != '1'
+
+  if (!DO_TRACKING) {
+    return
   }
-})
+  const tracker = create(__URL__, {
+    ignoreLocalhost: false,
+    detailed: true,
+    ignoreOwnVisits: false,
+  })
+
+  let stop: () => void
+
+  const attrs = attributes(true)
+
+  console.log('[TRACKING]', 'Daten die wir erhalten', attrs)
+
+  stop = tracker.record(__DOMAIN_ID__).stop
+
+  const bounced = true
+  let bouncedId = ''
+  const noBounce = () => {
+    console.log(
+      '[TRACKING]',
+      'NO-BOUNCE tracked',
+      'Du hast auf dieser Seite navigiert und sie nicht direkt verlassen.'
+    )
+    tracker.updateAction(bouncedId, { key: 'bounced', value: 0 })
+  }
+
+  tracker.action(
+    __BOUNCE_EVENT_ID__,
+    {
+      key: 'bounced',
+      value: 1,
+    },
+    (eventId: string) => {
+      bouncedId = eventId
+      if (!bounced) {
+        noBounce()
+      }
+    }
+  )
+  let first = true
+  // @ts-expect-error app is not typed
+  ctx.app.router.afterEach(() => {
+    if (first) {
+      first = false
+      return
+    }
+    console.log('tracking afterEach')
+    stop()
+    stop = tracker.record(__DOMAIN_ID__).stop
+
+    if (bounced) {
+      noBounce()
+    }
+  })
+
+  // Add web-vitals
+  webVitals.forEach(([key, getter]) => {
+    getter((metric) => {
+      tracker.action(__WEB_VITALS_EVENT_ID__, {
+        key,
+        value: metric.value,
+      })
+      console.log('[WEB-VITALS-TRACKING]', key, metric.value)
+    })
+  })
+}
+
+export default plugin
