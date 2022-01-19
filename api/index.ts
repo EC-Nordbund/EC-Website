5dfd86bfefbf0561bf9d45220fae258a593118bc
Sebastian Krüger <2pi_r2@gmx.de>
2022-01-19 15:57:57 +0100

    Update index.ts

--- api/index.ts
+++ api/index.ts
@@ -381,14 +381,14 @@ app.post('/confirm/:token', async (req, res) => {
                to: data.email,
                from: 'anmelung@ec-nordbund.de',
                subject: 'Anmeldung für Minderjährige Christival',
-               html: `<p>Moin,<br>Wenn du für Minderjährige Person(en) Verantwortlicher bist gebe den folgenden Code an diese weiter, damit sie sich auf der Website anmelden können! Bei Fragen zu dem Prozedere antworte einfach auf diese Mail!<br><br>CODE: ${key}</p>`
+               html: `<html lang="de"><p>Moin,<br>Wenn du für Minderjährige Person(en) Verantwortlicher bist gebe den folgenden Code an diese weiter, damit sie sich auf der Website anmelden können! Bei Fragen zu dem Prozedere antworte einfach auf diese Mail!<br><br>CODE: ${key}</p>`
              })
           } else {
             await sendMail({
                to: data.email,
                from: 'anmelung@ec-nordbund.de',
                subject: 'Christival | Einverständniserklärung der Eltern',
-               html: `<p>Moin,<br>Bitte lasse diese Erklärung von deinen Eltern unterschreiben und gebe sie an deiner volljährigen Begleitperson oder bei Thomas Seeger ab!</p>`,
+               html: `<html lang="de"><p>Moin,<br>Bitte lasse diese Erklärung von deinen Eltern unterschreiben und gebe sie an deiner volljährigen Begleitperson oder bei Thomas Seeger ab!</p>`,
                attachments: [
                  {
                    filename: 'Einverstaendniserklaerung.pdf',
