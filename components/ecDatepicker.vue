c2d9680eb0b52cc5278ab08f5f22ad7ce9fcf57f
Tobias <tobias_krause@akranet.de>
2022-01-25 17:10:28 +0100

    birthday picker rewrite

--- components/ecDatepicker.vue
+++ components/ecDatepicker.vue
@@ -1,59 +1,104 @@
-<template>
-    <v-dialog
-      v-model="menu2"
-      :close-on-content-click="false"
-      transition="scale-transition"
-      offset-y
-      max-width="290px"
-      min-width="auto"
-    >
-      <template v-slot:activator="{ on, attrs }">
-        <v-text-field
-          v-model="computedDateFormatted"
-          readonly
-          v-bind="{...attrs, ...$attrs}"
-          v-on="on"
-        ></v-text-field>
-      </template>
-      <v-date-picker
-        v-model="date"
-        no-title
-        @input="menu2 = false"
-      ></v-date-picker>
-    </v-dialog>
+<template lang="pug">
+  v-dialog(
+    v-model="menu"
+    :close-on-content-click="false"
+    transition="scale-transition"
+    offset-y
+    max-width="290px"
+    min-width="auto"
+  )
+    template( v-slot:activator="{ on, attrs }")
+      v-text-field(
+        v-model="formatedDate"
+        readonly
+        v-bind="{ max: maxDate, min: minDate, ...attrs, ...$attrs }"
+        v-on="on"
+        clearable
+        @click:clear="clear"
+      )
+    v-date-picker(
+      v-model="date"
+      :active-picker.sync="activePicker"
+      no-title
+      @input="menu = false"
+    )
 </template>
-<script>
-  export default {
-    data: vm => ({
-      date: '',
-      dateFormatted: null,
-      menu2: false,
-    }),
+<script lang="ts">
+import {
+  defineComponent,
+  computed,
+  ref,
+  Ref,
+  watchEffect,
+} from '@nuxtjs/composition-api'
 
-    computed: {
-      computedDateFormatted () {
-        return this.formatDate(this.date)
-      },
+export default defineComponent({
+  props: {
+    isBirthdayPicker: {
+      type: Boolean,
+      default: false,
     },
+  },
+  emits: ['input'],
+  setup: (props, { emit }) => {
+    const menu = ref(false)
+    const date: Ref<string> = ref('')
+    const activePicker = ref()
 
-    watch: {
-      date (val) {
-        this.dateFormatted = this.formatDate(this.date)
-        this.$emit('input', this.date)
-      },
-    },
+    const formatedDate = computed(() => formatDate(date.value))
 
-    methods: {
-      formatDate (date) {
-        if (!date) return null
+    const maxDate = computed(() => {
+      if (props.isBirthdayPicker) {
+        // today
+        return new Date(Date.now() - new Date().getTimezoneOffset() * 60000)
+          .toISOString()
+          .substr(0, 10)
+      }
 
-        return date.split('-').reverse().join('.')
-      },
-      parseDate (date) {
-        if (!date) return null
+      return undefined
+    })
 
-        return date.split('.').reverse().join('-')
-      },
-    },
-  }
+    const minDate = computed(() =>
+      props.isBirthdayPicker ? '1950-01-01' : undefined
+    )
+
+    watchEffect(() => {
+      if (menu.value && props.isBirthdayPicker) {
+        setTimeout(() => (activePicker.value = 'YEAR'))
+      }
+    })
+
+    watchEffect(function () {
+      return emit('input', date.value)
+    })
+
+    function formatDate(date: string) {
+      if (!date) return ''
+
+      return date.split('-').reverse().join('.')
+    }
+
+    function parseDate(date: string) {
+      if (!date) return null
+
+      return date.split('.').reverse().join('-')
+    }
+
+    function clear() {
+      menu.value = true
+      date.value = ''
+    }
+
+    return {
+      menu,
+      date,
+      formatedDate,
+      parseDate,
+      maxDate,
+      minDate,
+      activePicker,
+      clear
+    }
+  },
+})
 </script>
