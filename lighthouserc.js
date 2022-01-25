b9ae0442ec17b9d82f73e8b7a22d0d6543092e6d
Tobias <tobias_krause@akranet.de>
2022-01-25 22:12:24 +0100

    test lh no-pwa config

--- lighthouserc.js
+++ lighthouserc.js
@@ -15,33 +15,33 @@ module.exports = {
       // token: 'Your *build token* goes here', // could also use LHCI_TOKEN variable instead
     },
     assert: {
-      preset: 'lighthouse:recommended',
+      preset: 'lighthouse:no-pwa',
       assertions: {
-        'categories:performance': ['error', { minScore: 0.6 }],
-        'categories:accessibility': ['error', { minScore: 0.98 }],
-        'categories:pwa': ['error', { minScore: 0 }],
-        'categories:seo': ['error', { minScore: 0.92 }],
-        'categories:best-practices': ['error', { minScore: 1 }],
-        /**
-         * User Farbschema geht sonst nicht
-         * https://web.dev/color-contrast/?utm_source=lighthouse&utm_medium=cli
-         */
-        'color-contrast': 'off',
-        // Geht in CI nicht
-        'redirects-http': 'off',
-        'uses-http2': 'off',
-        // Zu viel unsed JS + CSS
-        'unused-css-rules': 'warn',
-        'unused-javascript': 'warn',
-        // Sollte bald wieder rein
-        'uses-optimized-images': 'warn',
-        'content-width': 'warn',
-        'non-composited-animations': 'warn',
-        // Probleme in CI
-        'canonical': 'off',
-        // NO PWA
-        'works-offline': 'off',
-        'offline-start-url': 'off'
+        // 'categories:performance': ['error', { minScore: 0.6 }],
+        // 'categories:accessibility': ['error', { minScore: 0.90 }],
+        // 'categories:pwa': ['error', { minScore: 0 }],
+        // 'categories:seo': ['error', { minScore: 0.92 }],
+        // 'categories:best-practices': ['error', { minScore: 1 }],
+        // /**
+        //  * User Farbschema geht sonst nicht
+        //  * https://web.dev/color-contrast/?utm_source=lighthouse&utm_medium=cli
+        //  */
+        // 'color-contrast': 'off',
+        // // Geht in CI nicht
+        // 'redirects-http': 'off',
+        // 'uses-http2': 'off',
+        // // Zu viel unsed JS + CSS
+        // 'unused-css-rules': 'warn',
+        // 'unused-javascript': 'warn',
+        // // Sollte bald wieder rein
+        // 'uses-optimized-images': 'warn',
+        // 'content-width': 'warn',
+        // 'non-composited-animations': 'warn',
+        // // Probleme in CI
+        // 'canonical': 'off',
+        // // NO PWA
+        // 'works-offline': 'off',
+        // 'offline-start-url': 'off'
       },
     },
   },
