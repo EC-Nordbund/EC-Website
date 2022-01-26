0d1a5382b70e4ada9f243febc6647a323fece753
Tobias Krause <39905423+tobikrs@users.noreply.github.com>
2022-01-26 01:21:44 +0100

    Update lighthouserc.js

--- lighthouserc.js
+++ lighthouserc.js
@@ -17,7 +17,7 @@ module.exports = {
     assert: {
       preset: 'lighthouse:no-pwa',
       assertions: {
-        'categories:performance': ['error', { minScore: 0.6 }],
+        'categories:performance': ['error', { minScore: 0.59 }],
         'categories:accessibility': ['error', { minScore: 0.92 }],
         'categories:seo': ['error', { minScore: 0.92 }],
         'categories:best-practices': ['error', { minScore: 0.92 }],
